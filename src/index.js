84f07ac22fe0655ae40eea65182a4592e32cf135
rivanildojr <rivanildo_junior7@hotmail.com>
2020-06-22 02:37:23 -0300

    desafio geral 01 - jogo-da-memoria

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const $root = document.querySelector("#root");
 
+const $game = document.createElement("main");
 const $memoryCard = document.createElement("article");
+const $memoryCardBack = document.createElement("article");
 
 const $icon = `<img 
     src='img/icon-collabcode.png' 
@@ -8,7 +10,24 @@ const $icon = `<img
     class='icon'
   />`;
 
+const $iconC = `<img 
+    src='img/icon-c.png' 
+    alt='Livro da Linguagem C++' 
+    class='icon'
+  />`;
+
+const $divCircle = `<div class='circle'>${$iconC}</div>`;
+
+$game.classList.add("game");
+$memoryCardBack.classList.add("memory-card", "-back");
 $memoryCard.classList.add("memory-card");
 
+$root.insertBefore($game, null);
+$root.insertBefore($memoryCardBack, null);
 $root.insertBefore($memoryCard, null);
+
+$game.insertAdjacentElement("afterbegin", $memoryCard);
+$game.insertAdjacentElement("afterbegin", $memoryCardBack);
+
+$memoryCardBack.insertAdjacentHTML("afterbegin", $divCircle);
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
