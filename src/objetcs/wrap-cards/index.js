dbc44b2065e09af01a8f2f538555744cf5cf87a2
rivanildojr <rivanildo_junior7@hotmail.com>
2020-06-23 03:01:24 -0300

    Desafio da aula 22

--- src/objetcs/wrap-cards/index.js
+++ src/objetcs/wrap-cards/index.js
@@ -1,3 +1,9 @@
 const $wrapCards = document.createElement("main");
 
 $wrapCards.classList.add("wrap-cards");
+
+$wrapCards.addEventListener("click", function (event) {
+  const target = event.target;
+
+  console.log(target);
+});
