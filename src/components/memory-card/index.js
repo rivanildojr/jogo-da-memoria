ce21e69dd4a74d85f3f422e31185f20ab78985c4
rivanildojr <rivanildo_junior7@hotmail.com>
2020-07-08 01:57:24 -0300

    Desafio da aula 26 - aplicando o styles pelo JS

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -24,6 +24,51 @@ const createMemoryCard = ({ nameClass, src, alt }) => `
     </article>
   `;
 
+function styles() {
+  const $head = document.querySelector("head");
+  const $styles = document.createElement("style");
+  $styles.textContent = `
+    .memory-card {
+      background-color: #f25a70;
+      border-radius: 30px;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+      height: 155px;
+      width: 155px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      cursor: pointer;
+    }
+
+    .memory-card > .icon {
+      width: 100px;
+    }
+
+    .memory-card.-front {
+      background-color: #fff;
+    }
+
+    .memory-card.-front::before {
+      content: "";
+      height: 94px;
+      width: 95px;
+      background-color: #d4d4d4;
+      border-radius: 50%;
+      position: absolute;
+    }
+
+    .memory-card.-front > .icon {
+      position: absolute;
+      transform: translateY(-12px);
+    }
+  `;
+
+  $head.insertAdjacentElement("beforeend", $styles);
+}
+
+styles();
+
 function handleClick() {
   console.log("ae");
 }
