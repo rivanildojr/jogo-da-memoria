2ee54f74d21e1da62b73dfb8676bbf7f9321c405
rivanildojr <rivanildo_junior7@hotmail.com>
2020-07-08 01:05:56 -0300

    Desafios da aula 25 - novos cards e default params

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,17 +2,43 @@ const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
 const $memoryCard = createMemoryCard();
-const $memoryCardFront = createMemoryCard(
+const $memoryCardC = createMemoryCard(
   "-front",
   "icon-c.png",
   "Icone do Livro da Linguagem C++"
 );
+const $memoryCardBug = createMemoryCard(
+  "-front",
+  "icon-bug.png",
+  "Icone de um bug"
+);
+const $memoryCardJs = createMemoryCard(
+  "-front",
+  "icon-js.png",
+  "Icone do Livro da Linguagem JS"
+);
+const $memoryCardJava = createMemoryCard(
+  "-front",
+  "icon-java.png",
+  "Icone do Livro da Linguagem Java"
+);
+const $memoryCardPhp = createMemoryCard(
+  "-front",
+  "icon-php.png",
+  "Icone do Livro da Linguagem PHP"
+);
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJs);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJs);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPhp);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPhp);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
-
-for (let index = 0; index < 6; index++) {
-  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-}
